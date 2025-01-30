5430cdda1f4d38b30886879a8e333a623acca23a
jnorton-7 <jnorton2@chicagobooth.edu>
2025-01-30 05:58:24 +0000

    Finished with 100 percent

--- app.rb
+++ app.rb
@@ -2,8 +2,73 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+erb(:homepage)
+
 end
+
+get("/rock") do
+ moves = ["rock", "paper", "scissors"]
+
+ @comp_move = moves.sample
+
+ if @comp_move == "rock"
+  @outcome = "tied"
+ end
+ 
+ if @comp_move == "paper"
+  @outcome = "lost"
+ end
+
+ if @comp_move == "scissors"
+  @outcome = "won"
+ end
+
+erb(:zebra)
+
+end
+
+
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+ 
+  @comp_move = moves.sample
+ 
+  if @comp_move == "paper"
+   @outcome = "tied"
+  end
+  
+  if @comp_move == "scissors"
+   @outcome = "lost"
+  end
+ 
+  if @comp_move == "rock"
+   @outcome = "won"
+  end
+ 
+ erb(:hippo)
+ 
+ end
+
+
+ get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+ 
+  @comp_move = moves.sample
+ 
+  if @comp_move == "scissors"
+   @outcome = "tied"
+  end
+  
+  if @comp_move == "rock"
+   @outcome = "lost"
+  end
+ 
+  if @comp_move == "paper"
+   @outcome = "won"
+  end
+ 
+ erb(:lion)
+ 
+ end
